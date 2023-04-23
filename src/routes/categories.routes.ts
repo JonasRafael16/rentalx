8a54217b66fedc1675d0e802a8ed5c62cf1b9753
Jonas Rafael de Lima <jonasrafael16@gmail.com>
2023-04-23 16:23:07 -0300

    Feat: add id in categorie

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
-
-  categories.push({
+  const category = {
     name,
     description,
-  });
+    id: uuidV4,
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
