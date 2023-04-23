e5472712448847a9a39b0f071a67a6876bee7127
Jonas Rafael de Lima <jonasrafael16@gmail.com>
2023-04-23 17:45:57 -0300

    Feat: create new service
    * using dependency inversion

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+
+import { CreateCategoryService } from '../services/CreateCategoryService';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
@@ -8,12 +10,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name)
-  if (categoryAlreadyExists) {
-    return response.status(400).json({error: "Category Already Exists"})
-  }
-  categoriesRepository.create({name, description});
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
 
+  createCategoryService.execute({name, description})
   return response.status(201).send();
 });
 
