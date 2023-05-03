ff399b3f10396b3d606429fe259c4efe7388d53e
Jonas Rafael de Lima <jonasrafael16@gmail.com>
2023-05-03 20:44:36 -0300

    Feat: add liskov substituion Principle

---
+++ src/repositories/ICategoriesRepository.ts
@@ -0,0 +1,15 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+interface ICategoriesRepository {
+  findByName(name: string): Category | void;
+  list(): Category[];
+  create({ name, description }: ICreateCategoryDTO): void;
+
+}
+
+export { ICategoriesRepository, ICreateCategoryDTO }
