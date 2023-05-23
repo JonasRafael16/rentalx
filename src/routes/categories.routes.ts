657424482aca73aed30ca08ee8b60d1513f3e7f9
Jonas Rafael de Lima <jonasrafael16@gmail.com>
2023-05-22 21:52:00 -0300

    Feat: create listCategories useCase

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
-
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  const all = categoriesRepository.list();
-  return response.json(all);
-})
+  return listCategoriesController.handle(request, response);
+});
 
 export { categoriesRoutes };
